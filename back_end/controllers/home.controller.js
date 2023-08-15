339c0336587cafa325421420bee4565912dbaa71
hoangak47 <hoang41911@gmail.com>
2023-08-15 23:25:20 +0700

    update

--- back_end/controllers/home.controller.js
+++ back_end/controllers/home.controller.js
@@ -2,61 +2,106 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
+const startPuppeteerSession = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  return { browser, page };
+};
+
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
 const home = {
-  getHome: async (req, res) => {
+  carousel: (html, $) => {
+    const carousel = [];
+
+    $(".owl-carousel a", html).each(async function () {
+      const img = $(this).find("img").attr("src");
+      const title = $(this).find(".name").text();
+      const episode_latest = $(this).find(".episode_latest").text().trim();
+      const link = $(this).attr("href").split("/")[4].slice(0, -5);
+
+      carousel.push({
+        img,
+        title,
+        episode_latest,
+        link,
+      });
+    });
+
+    return carousel;
+  },
+  latest_Episodes: (html, $) => {
+    const latest_Episodes = [];
+    $(`.ah_content .movies-list .movie-item`, html).each(function () {
+      const title = $(this).find(".name-movie").text().trim();
+      const link = $(this).find("a").attr("href").split("/")[4].slice(0, -5);
+      const img = $(this).find("img").attr("src");
+      const episode_latest = $(this).find(".episode-latest").text().trim();
+      const rating = $(this)
+        .find(".score span")
+        .text()
+        .replace("star", "")
+        .trim();
+
+      latest_Episodes.push({
+        title,
+        link,
+        img,
+        episode_latest,
+        rating,
+      });
+    });
+
+    return latest_Episodes;
+  },
+  dateSchedule: (html, $) => {
+    const dateSchedule = [];
+
+    $(".tab-lichchieu a", html).each(function () {
+      const day = $(this).find(".item-label").text().trim();
+
+      dateSchedule.push({
+        day,
+      });
+    });
+
+    return dateSchedule;
+  },
+  getHome: asyncHandler(async (req, res) => {
     try {
       const response = await axios.get(process.env.URL);
       const html = response.data;
       const $ = cheerio.load(html);
 
-      const carousel = [];
+      const carousel = home.carousel(html, $);
 
-      $(".owl-carousel a", html).each(async function () {
-        const img = $(this).find("img").attr("src");
-        const title = $(this).find(".name").text();
-        const episode_latest = $(this).find(".episode_latest").text().trim();
-        const link = $(this).attr("href").split("/")[4].slice(0, -5);
+      const latest_Episodes = home.latest_Episodes(html, $);
 
-        carousel.push({
-          img,
-          title,
-          episode_latest,
-          link,
+      const dateSchedule = home.dateSchedule(html, $);
+
+      const { browser, page } = await startPuppeteerSession();
+      await page.goto(process.env.URL);
+      //Wait for the page to be loaded
+
+      let schedule = await page.evaluate(async () => {
+        return await new Promise((resolve, reject) => {
+          const data = document.body.querySelectorAll(
+            "#LichChieuPhim .name-movie"
+          );
+
+          let title = [];
+
+          data.forEach((item) => {
+            title.push(item.innerText);
+          });
+
+          resolve(title);
         });
       });
 
-      const latest_Episodes = [];
-      $(`.ah_content .movies-list .movie-item`, html).each(function () {
-        const title = $(this).find(".name-movie").text().trim();
-        const link = $(this).find("a").attr("href").split("/")[4].slice(0, -5);
-        const img = $(this).find("img").attr("src");
-        const episode_latest = $(this).find(".episode-latest").text().trim();
-        const rating = $(this)
-          .find(".score span")
-          .text()
-          .replace("star", "")
-          .trim();
-
-        latest_Episodes.push({
-          title,
-          link,
-          img,
-          episode_latest,
-          rating,
-        });
-      });
-
-      const dateSchedule = [];
       const date = new Date();
       const today = date.getDay();
-
-      $(".tab-lichchieu a", html).each(function () {
-        const day = $(this).find(".item-label").text().trim();
-
-        dateSchedule.push({
-          day,
-        });
-      });
 
       res.status(200).send({
         msg: "Success",
@@ -72,6 +117,7 @@ const home = {
               dateSchedule,
               currentDay: today,
             },
+            data: schedule,
           },
         },
         timestamp: new Date().getTime(),
@@ -81,8 +127,8 @@ const home = {
         message: error,
       });
     }
-  },
-  test: async (req, res) => {},
+  }),
+  test: asyncHandler(async (req, res) => {}),
 };
 
 module.exports = home;
