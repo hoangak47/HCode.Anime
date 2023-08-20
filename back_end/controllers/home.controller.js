16a121fd5748311bae2dadc625cdd91d387ad0b4
hoangak47 <hoang41911@gmail.com>
2023-08-21 00:22:14 +0700

    up

--- back_end/controllers/home.controller.js
+++ back_end/controllers/home.controller.js
@@ -73,6 +73,7 @@ const home = {
   schedule: async () => {
     const browser = await puppeteer.launch({
       headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
 
