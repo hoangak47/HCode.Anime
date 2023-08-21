27d065480d933b058404e4ce55a235398e246e69
hoangak47 <hoang41911@gmail.com>
2023-08-21 22:07:54 +0700

    up

--- back_end/controllers/home.controller.js
+++ back_end/controllers/home.controller.js
@@ -203,9 +203,48 @@ const home = {
       });
     }
   },
-  test: async (req, res) => {
+  scrapeLogic: async (res) => {
+    console.log("Loading page");
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+    });
+
+    console.log("Browser opened");
+
     try {
-    } catch (error) {}
+      const page = await browser.newPage();
+
+      await page.goto("https://developer.chrome.com/");
+
+      // Type into search box
+      await page.type(".search-box__input", "automate beyond recorder");
+
+      // Wait and click on first result
+      const searchResultSelector = ".search-box__link";
+      await page.waitForSelector(searchResultSelector);
+      await page.click(searchResultSelector);
+
+      // Locate the full title with a unique string
+      const textSelector = await page.waitForSelector(
+        "text/Customize and automate"
+      );
+      const fullTitle = await textSelector.evaluate((el) => el.textContent);
+
+      // Print the full title
+      const logStatement = `The title of this blog post is ${fullTitle}`;
+      console.log(logStatement);
+      res.send(logStatement);
+    } catch (e) {
+      console.error(e);
+      res.send(`Something went wrong while running Puppeteer: ${e}`);
+    } finally {
+      await browser.close();
+    }
   },
 };
 
--- back_end/routers/home.router.js
+++ back_end/routers/home.router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
-const { getHome, test } = require("../controllers/home.controller");
+const { getHome, scrapeLogic } = require("../controllers/home.controller");
 
 router.get("/", getHome);
-router.get("/carousel", test);
+router.get("/carousel", scrapeLogic);
 
 module.exports = router;
