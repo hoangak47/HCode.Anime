c5890167c8a64391e5aae887446ea9f30f7fba40
hoangak47 <hoang41911@gmail.com>
2023-08-21 23:58:02 +0700

    up

--- back_end/controllers/home.controller.js
+++ back_end/controllers/home.controller.js
@@ -52,77 +52,83 @@ const home = {
 
     return latest_Episodes;
   },
-  dateSchedule: (html, $) => {
-    const dateSchedule = [];
+  getSchedule: async (page, day) => {
+    try {
+      await page.click(day);
 
-    $(".tab-lichchieu a", html).each(function () {
-      const day = $(this).find(".item-label").text().trim();
+      const waitDay = await page.waitForSelector("#LichChieuPhim .name-movie");
+      return await waitDay.evaluate(async () => {
+        const title_anime = document.body.querySelectorAll(
+          "#LichChieuPhim .name-movie"
+        );
 
-      dateSchedule.push({
-        day,
+        const link_anime = document.body.querySelectorAll("#LichChieuPhim a");
+
+        const episode_latest = document.body.querySelectorAll(
+          "#LichChieuPhim .episode-latest"
+        );
+
+        const rating = document.body.querySelectorAll(
+          "#LichChieuPhim .score span"
+        );
+
+        const img = document.body.querySelectorAll("#LichChieuPhim img");
+
+        const schedule = [];
+
+        for (let i = 0; i < title_anime.length; i++) {
+          const title = title_anime[i].innerText;
+          const link = link_anime[i].href.split("/")[4].slice(0, -5);
+          const episode = episode_latest[i].innerText;
+          const rating_anime = rating[i].innerText.replace("star", "").trim();
+          const img_anime =
+            img[i].src.split("/")[0] === "https:"
+              ? img[i].src
+              : `${process.env.URL + img[i].src}`;
+
+          schedule.push({
+            title,
+            link,
+            episode,
+            rating: rating_anime,
+            img: img_anime,
+          });
+        }
+
+        return schedule;
       });
-    });
-
-    return dateSchedule;
+    } catch (error) {
+      console.log(error);
+    }
   },
   schedule: async () => {
-    console.log("Loading page");
-    const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-    });
-    console.log("Browser opened");
+    const browser = await puppeteer.launch();
+
     const page = await browser.newPage();
-    console.log("Page created");
+
     await page.goto(process.env.URL);
-    console.log("Page loaded");
 
-    let schedule = await page.evaluate(async () => {
-      const title_anime = document.body.querySelectorAll(
-        "#LichChieuPhim .name-movie"
-      );
+    const schedule = [];
 
-      const link_anime = document.body.querySelectorAll("#LichChieuPhim a");
+    const monday = await home.getSchedule(page, "#thu-2");
+    const tuesday = await home.getSchedule(page, "#thu-3");
+    const wednesday = await home.getSchedule(page, "#thu-4");
+    const thursday = await home.getSchedule(page, "#thu-5");
+    const friday = await home.getSchedule(page, "#thu-6");
+    const saturday = await home.getSchedule(page, "#thu-7");
+    const sunday = await home.getSchedule(page, "#thu-8");
 
-      const episode_latest = document.body.querySelectorAll(
-        "#LichChieuPhim .episode-latest"
-      );
+    schedule.push(
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+      saturday,
+      sunday
+    );
 
-      const rating = document.body.querySelectorAll(
-        "#LichChieuPhim .score span"
-      );
-
-      const img = document.body.querySelectorAll("#LichChieuPhim img");
-
-      const schedule = [];
-
-      for (let i = 0; i < title_anime.length; i++) {
-        const title = title_anime[i].innerText;
-        const link = link_anime[i].href.split("/")[4].slice(0, -5);
-        const episode = episode_latest[i].innerText;
-        const rating_anime = rating[i].innerText.replace("star", "").trim();
-        const img_anime =
-          img[i].src.split("/")[0] === "https:"
-            ? img[i].src
-            : `${process.env.URL + img[i].src}`;
-
-        schedule.push({
-          title,
-          link,
-          episode,
-          rating: rating_anime,
-          img: img_anime,
-        });
-      }
-
-      return schedule;
-    });
-
-    console.log("Schedule loaded");
+    await browser.close();
 
     return schedule;
   },
@@ -161,9 +167,6 @@ const home = {
 
       const latest_Episodes = home.latest_Episodes(html, $).slice(0, 30);
 
-      const dateSchedule = home.dateSchedule(html, $);
-
-      console.log("Schedule loaded");
       const schedule = await home.schedule();
 
       const date = new Date();
@@ -181,10 +184,7 @@ const home = {
           },
           schedule: {
             title: "Lịch chiếu",
-            date: {
-              dateSchedule,
-              currentDay: today,
-            },
+            currentDay: today - 1,
             data: schedule,
           },
           comingSoon: {
