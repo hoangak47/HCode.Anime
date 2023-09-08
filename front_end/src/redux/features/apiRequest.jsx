08cece4c8b3ea6f68eae3852a8f709b22acebd41
hoangak47 <hoang41911@gmail.com>
2023-09-08 23:54:00 +0700

    update

--- front_end/src/redux/features/apiRequest.jsx
+++ front_end/src/redux/features/apiRequest.jsx
@@ -22,7 +22,7 @@ import { getSeeAllMovieFailure, getSeeAllMovieStart, getSeeAllMovieSuccess } fro
 export const getHomeData = async (dispatch, axios) => {
     try {
         dispatch(getDataStart());
-        const response = await axios.get('/api/home');
+        const response = await axios.get('http://localhost:5000/api/home');
         dispatch(getDataSuccess(response.data));
     } catch (error) {
         console.log(error);
